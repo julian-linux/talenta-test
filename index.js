a1ff219f843d5c83310070031e4f481437bf7df4
julian mosquera <julian.linux@gmail.com>
2020-04-29 15:10:26 -0500

    some fixes

--- index.js
+++ index.js
@@ -1,80 +1,113 @@
 const https = require('https')
+const http = require('http')
 
 const planetsRequested = {}
 const actorsRequested = {}
+const speciesRequested = {}
+const starshipsRequested = {}
 
-const requestApi = (url, callback) => https.get(url, response => {
-  let data = ''
-  response.on('data', chunk => data += chunk);
-  response.on('end', () => callback(JSON.parse(data)));
+const requestApi = url => new Promise(resolve => {
+  https.get(url, response => {
+    let data = ''
+    response.on('data', chunk => data += chunk);
+    response.on('end', () => resolve(JSON.parse(data)));
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  })
+
 })
 
-
 // request array of url in api and store result
-const requestElement = (element, storage, storeKeys) => new Promise((resolve) => {
+const requestElement = async (element, storage, storeKeys) => new Promise((resolve) => {
   const requestedElements = []
-  // get object data, if not exists in storage... request element data and store it
-  const _requestElement = (idx = 0) => {
-    if (element[idx]) {
+
+  try {
+     // get object data, if not exists in storage... request element data and store it
+  const requestApiElement = async (idx = 0) => {
+    if (typeof element[idx] === 'string') {
       if (!storage[element[idx]]) {
-        requestApi(element[idx].replace('http', 'https'), data => {
-          console.log('-------------------data', storeKeys, data);
-          const storeData = storeKeys.map(key => data[key])
-          storage[element[idx]] = storeData
-          requestedElements.push(storeData)
-          requestElement(++idx)
-        })
+        console.info(`--requesting ${element[idx]}`)
+        const data = await requestApi(element[idx].replace('http', 'https'))
+        
+        const storeData = storeKeys.reduce((prev, key) => ({ ...prev, [key]: data[key] }), {})
+        storage[element[idx]] = storeData
+        requestedElements.push(storeData)
+        await requestApiElement(++idx)
       } else {
         requestedElements.push(storage[element[idx]])
-        requestElement(++idx)
+
+        await requestApiElement(++idx)
       }
     } else {
       resolve(requestedElements)
     }
   }
-  _requestElement()
+  requestApiElement()
+  } catch (error) {
+    console.error('error ')
+  }
+ 
 })
 
 
-const parseFilmsData = data => {
+const parseFilmsData = async data => {
+  console.info('init parseFilms');
+  const response = []
   const planetKeys = ['name', 'terrain', 'gravity', 'diameter', 'population']
-  const actorKeys = ['name', 'gender', 'hair_color', 'skin_color', 'eye_color', 'height', 'homeworld']
+  const actorKeys = ['name', 'gender', 'hair_color', 'skin_color', 'eye_color', 'height', 'homeworld', 'species']
+  const specieKeys = ['name', 'language', 'average_height']
+  const starshipKeys = ['name', 'model', 'manufacturer', 'passengers', 'length']
 
-  return data.results.reduce(async (prev, actual) => {
-    try {
-      const { title, planets: _planets, characters } = actual // get parent objects
-      const planets = await requestElement(_planets, planetsRequested, planetKeys)
-      
-      const actors = await requestElement(characters, actorsRequested, actorKeys)
-      return [
-        ...prev,
-        {
-          title,
-          planets,
-          actors
-        }
-      ]
-    } catch (error) {
-      console.error('error iterating' + error.message)
+  for (const { title, planets: _planets, characters, starships: _starships } of data.results) {
+    console.info(`-getting planets of -${title}-`);
+    const planets = await requestElement(_planets, planetsRequested, planetKeys)
+
+    console.info(`-getting actors of -${title}-`);
+    const actors = await requestElement(characters, actorsRequested, actorKeys)
+
+    for (const actor of actors) {
+      if (actor.species.length) {
+        console.info(`--getting species of -${actor.name}-`);
+        actor.species = await requestElement(actor.species, speciesRequested, specieKeys)
+      }
     }
 
-  }, [])
+    console.info(`-getting starships of -${title}-`);
+    const starships = await requestElement(_starships, starshipsRequested, starshipKeys)
+ 
+    console.info(`-checking bigger starship`);
+    const biggerStarship = Object.values(starships).reduce((prev, actual) => parseFloat(prev.length) > parseFloat(actual.length) ? prev : actual ,{})
+
+
+    response.push({
+      title,
+      planets,
+      actors,
+      starships,
+      biggerStarship
+    })
+  }
+
+  return response
 }
 
-
-
-
-const requestFilms = () => requestApi('https://swapi.dev/api/films/', parseFilmsData)
-
-const runAPI = () => {
-  requestFilms();
+const requestFilms = async () => {
+  const data = await requestApi('https://swapi.dev/api/films/')
+  return await parseFilmsData(data)
 }
 
-// https.createServer((req, res) => {
-//   res.end(runAPI())
-// })
+const runAPI = async () => await requestFilms();
 
-runAPI();
+http.createServer(async (_req, res) => {
+
+  const data = await runAPI()
+  // console.info('##FINISHED!##');
+  res.setHeader('Connection', 'Transfer-Encoding');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Transfer-Encoding', 'chunked');
+  res.write(JSON.stringify(data));
+  res.end()
+  }).listen(3000)
+
+// runAPI();
